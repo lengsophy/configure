40a6e80d42d72b0cb55b239882ccf65ed853a85d
lengsophy <you@example.com>
2016-02-29 18:22:38 +0700

    ee

--- setupCentOS.sh
+++ setupCentOS.sh
@@ -5,6 +5,8 @@ yum groupinstall "Development Tools"
 wait
 yum install wget
 wait
+sudo yum install epel-release
+wait
 yum install sudo gcc-c++
 wait
 wget http://nodejs.org/dist/v0.10.35/node-v0.10.35.tar.gz
@@ -19,14 +21,17 @@ make
 wait
 sudo make install
 wait
+sudo yum install npm
+wait
 sudo npm install -g n
 wait
 sudo n 0.10.40
 wait
 cd ..
-sudo yum install npm
 wait
 sudo npm install pm2 -g
+wait
+curl install.meteor.com | /bin/sh
 wait
 sudo cp mongodb-org-3.0.repo /etc/yum.repos.d/
 wait 
